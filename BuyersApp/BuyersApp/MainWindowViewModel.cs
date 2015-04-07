486a6d8523b51cf11cb17a172cce568cdd2246a7
Reka Burmeister <reka.burmeister@gmail.com>
2015-04-07 07:18:44 +0100

    Countries autocomplete

---
+++ BuyersApp/BuyersApp/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using BuyersApp.Annotations;
+
+namespace BuyersApp
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public IEnumerable<string> Countries { get {return new []{"UK", "US", "China"};} }
+        public string SelectedCountry { get; set; }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
