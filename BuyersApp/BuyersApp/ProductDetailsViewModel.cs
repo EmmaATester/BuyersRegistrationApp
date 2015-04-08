6acd985e4d43cdc206e618007760df482bdd6480
EmmaArmstrong <emma_deakin@hotmail.com>
2015-04-08 07:56:41 +0100

    updated combo box for product details
    Updated combo box for product details

---
+++ BuyersApp/BuyersApp/ProductDetailsViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using BuyersApp.Annotations;
+
+namespace BuyersApp
+{
+    class ProductDetailsViewModel : INotifyPropertyChanged
+    {
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+     
+
+        public IEnumerable<string> Categories { get { return new[] { "Music", "book", "China" }; } }
+        public string SelectedCategory { get; set; }
+
+       
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
