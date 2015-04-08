1b60efeba2631d635fca54097b87c8469caae013
EmmaArmstrong <emma_deakin@hotmail.com>
2015-04-08 09:44:41 +0100

    Bindings

--- BuyersApp/BuyersApp/ProductDetails.xaml.cs
+++ BuyersApp/BuyersApp/ProductDetails.xaml.cs
@@ -20,9 +20,42 @@ namespace BuyersApp
     /// </summary>
     public partial class ProductDetails : Page
     {
+
+        
         public ProductDetails()
         {
             InitializeComponent();
+        }
+
+        private void DatePicker_OnSelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+           
+
+            // ... Get DatePicker reference.
+            var picker = sender as DatePicker;
+
+            // ... Get nullable DateTime from SelectedDate.
+            DateTime? date = picker.SelectedDate;
+            if (date == null)
+            {
+                // ... A null object.
+                MessageBox.Show("A handled exception just occurred: " , "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                // ... No need to display the time.
+                this.Title = date.Value.ToShortDateString();
+            }
+
+            var selectedDateYear = new ComboBoxItem();
+            selectedDateYear.Content = date.Value.Year;
+            YearComboBox.Items.Add(selectedDateYear);
+            YearComboBox.SelectedIndex = 0;
+
+            //       DayComboBox.SelectedValue = date.Value.Day;
+            //   MonthComboBox.SelectedValue = date.Value.Month;
+
+
         }
     }
 }
--- BuyersApp/BuyersApp/ProductDetailsViewModel.cs
+++ BuyersApp/BuyersApp/ProductDetailsViewModel.cs
@@ -5,21 +5,38 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Documents;
 using BuyersApp.Annotations;
 
 namespace BuyersApp
 {
     class ProductDetailsViewModel : INotifyPropertyChanged
     {
+        private DateTime _selectedDate;
 
 
         public event PropertyChangedEventHandler PropertyChanged;
-     
+
+        public DateTime selectedDate
+        {
+            get { return _selectedDate; }
+            set { _selectedDate = value; }
+        }
+
+       // int     YearComboBox.SelectedValue = date.Value.Year;
+   //    int     DayComboBox.SelectedValue = date.Value.Day;
+      // int     MonthComboBox.SelectedValue = date.Value.Month;
 
         public IEnumerable<string> Categories { get { return new[] { "Music", "book", "China" }; } }
         public string SelectedCategory { get; set; }
 
-       
+        public IEnumerable<int> Years
+        {
+            get { return Enumerable.Range(1930, 81).ToList(); }
+        }
+    
+    
+   
 
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
@@ -27,5 +44,8 @@ namespace BuyersApp
             PropertyChangedEventHandler handler = PropertyChanged;
             if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
         }
+
+
+
     }
 }
