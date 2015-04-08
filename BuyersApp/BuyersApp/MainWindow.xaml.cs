21f074a780015dcf704fb64708c3d2d4762aa738
Reka Burmeister <reka.burmeister@gmail.com>
2015-04-08 06:35:14 +0100

    A better way of doing the navigation

--- BuyersApp/BuyersApp/MainWindow.xaml.cs
+++ BuyersApp/BuyersApp/MainWindow.xaml.cs
@@ -18,12 +18,47 @@ namespace BuyersApp
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : NavigationWindow
+    public partial class MainWindow : Window
     {
+        private readonly List<Page> m_Pages = new List<Page> { new FirstPage(), new ProductDetails() };
+        private int m_CurrentPage = 0;
+
         public MainWindow()
         {
             InitializeComponent();
-            Navigate(new FirstPage());
+            BackButton.IsEnabled = false;
+            PageFrame.Navigate(m_Pages[m_CurrentPage]);
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (m_CurrentPage + 1 == m_Pages.Count)
+            {
+                // Run thing
+                Close();
+            }
+            else
+            {
+                m_CurrentPage++;
+                PageFrame.Navigate(m_Pages[m_CurrentPage]);
+                BackButton.IsEnabled = true;
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            m_CurrentPage--;
+            if (m_CurrentPage == 0)
+            {
+                BackButton.IsEnabled = false;
+            }
+            PageFrame.Navigate(m_Pages[m_CurrentPage]);
+
         }
     }
 }
