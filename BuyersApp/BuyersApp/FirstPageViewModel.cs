bca88dc326c5a14f24e27970914f3acb12735eac
Reka Burmeister <reka.burmeister@gmail.com>
2015-04-08 07:21:46 +0100

    Countries autocomplete

--- BuyersApp/BuyersApp/FirstPageViewModel.cs
+++ BuyersApp/BuyersApp/FirstPageViewModel.cs
@@ -12,15 +12,28 @@ namespace BuyersApp
     class FirstPageViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+        private string[] m_Countries;
 
-        public IEnumerable<string> Countries { get {return new []{"UK", "US", "China"};} }
+        public IEnumerable<string> Countries { get {return m_Countries;} }
         public string SelectedCountry { get; set; }
+
+        public FirstPageViewModel()
+        {
+            GetCountries();
+        }
 
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChangedEventHandler handler = PropertyChanged;
             if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void GetCountries()
+        {
+            string fileContent = Properties.Resources.Countries;
+            m_Countries = fileContent.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
         }
     }
 }
